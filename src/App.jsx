32e704fc2e4100c36c953e4e8887937822fb6cac
KotDenisV <kot.denis@gmail.com>
2024-07-28 23:30:42 +0300

    structure App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import MoviesPage from './pages/MoviesPage/MoviesPage';
+import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import MovieReviews from './components/MovieReviews/MovieReviews';
+import MovieCast from './components/MovieCast/MovieCast';
 
 function App() {
   
 
-  return (
-    <>
+  return (    
       <div>
-        <p>Привіт!</p>
-      </div>
-    </>
+        <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/movies' element={<MoviesPage />} />
+        <Route path='/movies/:movieId' element={<MovieDetailsPage />} >
+          <Route path='cast' element={<MovieCast />} />
+          <Route path='reviews' element={<MovieReviews />} />
+        </Route>
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+      </div>    
   )
 }
 
---
+++ src/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNzJhZjM3ZDMyMjkwYTlhMWRiNjBiZmE3NjBkOWViNCIsIm5iZiI6MTcyMjE4MzkxMC4yMDkwMTYsInN1YiI6IjY2YTY2YjUwZWMzN2RkOWMzODNhMTRkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BTDAs8JDmIs4C2ct0x7Zh12LDaDH7CQZJHLuLpUmwh0';
+const options = {
+  headers: {	
+    Authorization: `Bearer ${accessToken}`
+  }
+};
+
+const getTrendingMovies = async () => {
+  const responce = await axios.get('trending/movie/day', options);
+  return responce.data.results;
+};
+
+export {
+  getTrendingMovies  
+};
